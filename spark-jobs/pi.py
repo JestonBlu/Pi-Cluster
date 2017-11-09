d265579d19d2fefa5292d2e404e54f095435d77f
JestonBlu <jestonblu@gmail.com>
2017-11-09 16:19:28 -0600

    added spark example

---
+++ spark-jobs/pi.py
@@ -0,0 +1,39 @@
+'''
+This script is from the spark examples folder, slightly modified. I modified
+this script in order to be able to run it interactively in Atom with Hydrogen.
+In order for spark to work within Atom I needed to bind the address and set
+master. I think hardcoded the partitions so that it could be run interactively
+or by using spark-submit
+'''
+
+from __future__ import print_function
+import sys
+from random import random
+from operator import add
+
+from pyspark.sql import SparkSession
+
+partitions = 1000
+
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .master("local[6]") \
+        .config("spark.driver.bindAddress", "127.0.0.1") \
+        .appName("PythonPi")\
+        .getOrCreate()
+
+    n = 100000 * partitions
+
+    def f(_):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+
+    spark.stop()
