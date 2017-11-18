c07b75c3b7f9aaa9e7b78af53bb2994cc8969296
Jeston <jestonblu@gmail.com>
2017-11-18 03:31:14 +0000

    update

--- scripts/jeb-build-repo.sh
+++ scripts/jeb-build-repo.sh
@@ -1,1 +1,1 @@
-sudo repo-add /var/cache/pacman/pkg/cluster.repo.db.tar.gz /var/cache/pacman/pkg/*.pkg.tar.xz
+sudo repo-add -n /var/cache/pacman/pkg/cluster.repo.db.tar.gz /var/cache/pacman/pkg/*.pkg.tar.xz
--- scripts/jeb-rsync-apps.sh
+++ scripts/jeb-rsync-apps.sh
@@ -1,3 +1,3 @@
-rsync -razP --progress ~/apps/ rpi1:~/apps &
 rsync -razP --progress ~/apps/ rpi2:~/apps &
+rsync -razP --progress ~/apps/ rpi3:~/apps &
 rsync -razP --progress ~/apps/ rpi4:~/apps
