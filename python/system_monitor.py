b7a0c60f3834bd717d021289a0e68f336f8dfe65
jestonblu <jestonblu@gmail.com>
2017-11-19 20:43:52 -0600

    update

--- python/system_monitor.py
+++ python/system_monitor.py
@@ -2,10 +2,24 @@
 '''
 
 from pyspark.sql import SparkSession
-import pandas as import pd
+import pandas as pd
+import matplotlib.pyplot as plt
 
 spark = SparkSession \
     .builder \
     .config("spark.driver.bindAddress", "127.0.0.1") \
     .appName("PythonPi") \
     .getOrCreate()
+
+# Read in data
+dta = pd.read_csv("data/rpi_stats.csv")
+
+# Make date field the index
+dta['date'] = dta['date'].apply(pd.to_datetime)
+dta.index = dta['date']
+dta = dta[['rpi1', 'rpi2', 'rpi3', 'rpi4']]
+
+# Calculate mean over time
+dta.resample('10min').mean()
+
+plt.plot(dta)
