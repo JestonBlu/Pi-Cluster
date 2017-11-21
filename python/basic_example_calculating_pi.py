28d2f0f12b56000a1cde1950de7a49de9c9b1163
Jeston <JestonBlu@users.noreply.github.com>
2017-11-21 15:06:46 -0600

    Update basic_example_calculating_pi.py

--- python/basic_example_calculating_pi.py
+++ python/basic_example_calculating_pi.py
@@ -29,7 +29,6 @@ def f(_):
     return 1 if x ** 2 + y ** 2 <= 1 else 0
 
 
-count = spark.sparkContext.parallelize(
-    range(1, n + 1), partitions).map(f).reduce(add)
+count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
 
 print("Pi is roughly %f" % (4.0 * count / n))
