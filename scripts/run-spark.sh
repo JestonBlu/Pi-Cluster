255b544b33a560e85090dc5574d8cfb0f2088c9b
Jeston <jestonblu@gmail.com>
2018-01-28 21:24:24 -0600

    automated commit

--- scripts/run-spark.sh
+++ scripts/run-spark.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /home/jeston/projects/pi-cluster &&
-/home/jeston/apps/spark-2.2/bin/spark-submit /home/jeston/projects/pi-cluster/python/system_monitor.py > /home/jeston/projects/pi-cluster/logs/output.log &
+cd /home/jeston/projects/pi-cluster
+/home/jeston/apps/spark-2.2/bin/spark-submit /home/jeston/projects/pi-cluster/python/system_monitor.py > /home/jeston/projects/pi-cluster/logs/output.log
