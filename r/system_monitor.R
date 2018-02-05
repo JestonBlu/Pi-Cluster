9b582dff3329e95204097e810a300a83cad29520
jestonblu <jestonblu@gmail.com>
2018-02-05 17:42:00 -0600

    update

--- r/system_monitor.R
+++ r/system_monitor.R
@@ -3,7 +3,7 @@ library(reshape2)
 library(scales)
 library(gridExtra)
 
-x = read.csv("/home/jeston/Projects/pi-cluster/data/rpi_stats.csv")
+x = read.csv("/home/jeston/projects/pi-cluster/data/rpi_stats.csv")
 x = as.data.frame(x)
 x = na.omit(x)
 x = melt(x, id.vars = "date")
@@ -41,7 +41,7 @@ g2 = ggplot(subset(x, typ == 'CPU % Used')) +
   geom_line(aes(x = date, y = value, color = rpi)) +
   scale_x_datetime("") +
   scale_y_continuous("") +
-  scale_color_discrete("") +
+  scale_color_discrete("", guide = FALSE) +
   facet_wrap(~typ, scales = "free") +
   theme(text = element_text(family = "mono"),
       plot.title = element_text(hjust = .5),
@@ -51,12 +51,12 @@ g3 = ggplot(subset(x, typ == 'RAM % Used')) +
   geom_line(aes(x = date, y = value, color = rpi)) +
   scale_x_datetime("") +
   scale_y_continuous("") +
-  scale_color_discrete("") +
+  scale_color_discrete("", guide = FALSE) +
   facet_wrap(~typ, scales = "free") +
   theme(text = element_text(family = "mono"),
         plot.title = element_text(hjust = .5),
         plot.subtitle = element_text(hjust = .5))
 
-ggsave("/home/jeston/Projects/pi-cluster/output/cpu.png",
+ggsave("/home/jeston/projects/pi-cluster/output/cpu.png",
   grid.arrange(g1, arrangeGrob(g2, g3, nrow = 1), nrow = 2),
   width = 7, height = 5)
