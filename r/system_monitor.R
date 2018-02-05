f5b832b5d456de889156aeab5927451028249811
jestonblu <jestonblu@gmail.com>
2018-02-05 17:38:47 -0600

    update

--- r/system_monitor.R
+++ r/system_monitor.R
@@ -3,7 +3,7 @@ library(reshape2)
 library(scales)
 library(gridExtra)
 
-x = read.csv("/home/jeston/projects/pi-cluster/data/rpi_stats.csv")
+x = read.csv("/home/jeston/Projects/pi-cluster/data/rpi_stats.csv")
 x = as.data.frame(x)
 x = na.omit(x)
 x = melt(x, id.vars = "date")
@@ -23,7 +23,10 @@ x$typ = ifelse(substr(x$variable, 6, 13) == "mem_pct", "RAM % Used",  x$typ)
 x$date = substr(x$date, 1, 19)
 x$date = as.POSIXlt(x$date)
 
-g1 = ggplot(x) +
+x$rpi = factor(x$rpi)
+x$typ = factor(x$typ)
+
+g1 = ggplot(subset(x, typ == 'CPU Temp')) +
   geom_line(aes(x = date, y = value, color = rpi)) +
   scale_x_datetime("") +
   scale_y_continuous("") +
@@ -34,4 +37,26 @@ g1 = ggplot(x) +
     plot.subtitle = element_text(hjust = .5)) +
   ggtitle("Raspber Pi Cluster Stats", "(Last 24 Hours)")
 
-ggsave("/home/jeston/projects/pi-cluster/output/cpu.png", g1, width = 7, height = 5)
+g2 = ggplot(subset(x, typ == 'CPU % Used')) +
+  geom_line(aes(x = date, y = value, color = rpi)) +
+  scale_x_datetime("") +
+  scale_y_continuous("") +
+  scale_color_discrete("") +
+  facet_wrap(~typ, scales = "free") +
+  theme(text = element_text(family = "mono"),
+      plot.title = element_text(hjust = .5),
+      plot.subtitle = element_text(hjust = .5))
+
+g3 = ggplot(subset(x, typ == 'RAM % Used')) +
+  geom_line(aes(x = date, y = value, color = rpi)) +
+  scale_x_datetime("") +
+  scale_y_continuous("") +
+  scale_color_discrete("") +
+  facet_wrap(~typ, scales = "free") +
+  theme(text = element_text(family = "mono"),
+        plot.title = element_text(hjust = .5),
+        plot.subtitle = element_text(hjust = .5))
+
+ggsave("/home/jeston/Projects/pi-cluster/output/cpu.png",
+  grid.arrange(g1, arrangeGrob(g2, g3, nrow = 1), nrow = 2),
+  width = 7, height = 5)
