b14fa96531d2a2cfab64a825320d961bcce8b6b2
JestonBlu <jestonblu@gmail.com>
2018-03-10 07:52:56 -0600

    updated

--- scripts/run-spark.sh
+++ scripts/run-spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-/home/jeston/apps/spark-2.2/bin/spark-submit /home/jeston/projects/pi-cluster/python/system_monitor.py > /home/jeston/projects/pi-cluster/logs/output.log &
+/home/jeston/apps/spark-2.2/bin/spark-submit /home/jeston/projects/pi-cluster/python/system_monitor.py > /home/jeston/projects/pi-cluster/logs/output.log
